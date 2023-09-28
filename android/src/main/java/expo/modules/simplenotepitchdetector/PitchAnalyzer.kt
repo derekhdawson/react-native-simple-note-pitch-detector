7abbb0e106119c7d3229cdfd222ef4fa8b12cc5a
ddawson <ddawson@tableau.com>
2023-09-27 23:17:06 -0700

    finally got it working

---
+++ android/src/main/java/expo/modules/simplenotepitchdetector/PitchAnalyzer.kt
@@ -0,0 +1,62 @@
+package expo.modules.simplepitchdetector
+
+import be.tarsos.dsp.AudioDispatcher
+import be.tarsos.dsp.AudioProcessor
+import be.tarsos.dsp.io.android.AudioDispatcherFactory
+import be.tarsos.dsp.pitch.PitchDetectionHandler
+import be.tarsos.dsp.pitch.PitchProcessor
+import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.WritableNativeMap
+import kotlin.math.log2
+import kotlin.math.round
+
+
+class PitchAnalyzer {
+
+    var onChangePitch: (String, Double) -> Unit
+
+    constructor(callback: (String, Double) -> Unit) {
+        this.onChangePitch = callback
+    }
+
+    private val tones = arrayOf("C","C#","D","D#","E","F","F#","G","G#","A","A#","B")
+
+    private var dispatcher: AudioDispatcher? = null
+    private var processor: AudioProcessor? = null
+
+    private var runner: Thread? = null
+
+    private val handler = PitchDetectionHandler { res, e ->
+        val pitchInHz = res.pitch
+        process(pitchInHz)
+    }
+
+    private fun prepare() {
+        processor =
+            PitchProcessor(PitchEstimationAlgorithm.FFT_YIN, 22050f, 1024, handler)
+        dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
+        dispatcher?.addAudioProcessor(processor)
+    }
+
+    private fun process(pitchInHz: Float) {
+        val index = round(12 * (log2(pitchInHz / 440) / log2(2f)) + 69) % 12
+
+        if (!index.isNaN() && pitchInHz > 0) {
+            val tone = tones[index.toInt()]
+            val frequency = pitchInHz.toDouble()
+
+            this.onChangePitch(tone, frequency)
+        }
+    }
+
+    fun start() {
+        prepare()
+        runner = Thread(dispatcher)
+        runner?.start()
+    }
+    fun stop() {
+        dispatcher?.stop()
+        runner?.interrupt()
+    }
+}
--- android/src/main/java/expo/modules/simplenotepitchdetector/ReactNativeSimpleNotePitchDetectorModule.kt
+++ android/src/main/java/expo/modules/simplenotepitchdetector/ReactNativeSimpleNotePitchDetectorModule.kt
@@ -1,47 +1,45 @@
 package expo.modules.simplenotepitchdetector
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.simplepitchdetector.PitchAnalyzer
+import androidx.core.os.bundleOf
 
 class ReactNativeSimpleNotePitchDetectorModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
+
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ReactNativeSimpleNotePitchDetector')` in JavaScript.
     Name("ReactNativeSimpleNotePitchDetector")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
+    Events("onChangePitch")
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+    Function("start") {
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ReactNativeSimpleNotePitchDetectorView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ReactNativeSimpleNotePitchDetectorView, prop: String ->
-        println(prop)
+      val pitchAnalyzer = PitchAnalyzer { tone: String, frequency: Double ->
+        this@ReactNativeSimpleNotePitchDetectorModule.sendEvent(
+          "onChangePitch",
+          bundleOf("tone" to tone, "frequency" to frequency)
+        )
       }
+
+      pitchAnalyzer.start()
+
+//      val context = appContext.reactContext!!
+//      var activity = appContext.currentActivity!!
+//      var permissionsGranted = ActivityCompat.checkSelfPermission(context, permissions[0]) == PackageManager.PERMISSION_GRANTED
+//      if (!permissionsGranted) {
+//        ActivityCompat.requestPermissions(activity, permissions, 200)
+//      } else {
+//        val pitchAnalyzer = PitchAnalyzer {
+//          this@ReactNativeSimpleNotePitchDetectorModule.sendEvent(
+//            "onChange",
+//            bundleOf("tone" to it)
+//          )
+//        }
+//        pitchAnalyzer.start()
+//      }
     }
   }
 }
