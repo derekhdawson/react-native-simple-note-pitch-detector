dd230f7fb8a7174cb32e185cbed00427b28b818b
ddawson <ddawson@tableau.com>
2023-10-08 16:08:24 -0700

    change pitch strategy

--- ios/ReactNativeSimpleNotePitchDetectorModule.swift
+++ ios/ReactNativeSimpleNotePitchDetectorModule.swift
@@ -4,12 +4,33 @@ import Pitchy
 
 public class ReactNativeSimpleNotePitchDetectorModule: Module {
     
+    var bufferSize = 3
+    var notes = Array(repeating: "", count: 3)
+    var counter = 0
+    
     lazy var pitchEngine: PitchEngine = { [weak self] in
         let config = Config(estimationStrategy: .yin)
         let pitchEngine = PitchEngine(config: config, delegate: self)
         pitchEngine.levelThreshold = -30
         return pitchEngine
     }()
+    
+    func mostFrequent(array: [String]) -> String? {
+        var counts = [String: Int]()
+
+        // Count the values with using forEach
+        array.forEach { counts[$0] = (counts[$0] ?? 0) + 1 }
+
+        // Find the most frequent value and its count with max(by:)
+        if let (value, count) = counts.max(by: {$0.1 < $1.1}) {
+            if (count <= 1) {
+                return nil
+            }
+            return value
+        }
+
+        return nil
+    }
     
     var isRecording = false
 
@@ -45,8 +66,24 @@ extension ReactNativeSimpleNotePitchDetectorModule: PitchEngineDelegate {
         guard absOffsetPercentage > 1.0 else {
             return
         }
+        
+        if (counter == bufferSize) {
+            var result = mostFrequent(array: notes)
+            if (result != nil) {
+                self.sendEvent("onChangePitch", ["note" :  result, "soundPressure" : ""])
+            }
+            counter = 0
+        }
+        
+        do {
+            let note = try Note(frequency: pitch.wave.frequency)
+            notes[counter] = note.string
+            counter += 1
+        } catch {
+            
+        }
 
-        self.sendEvent("onChangePitch", ["note" :  pitch.note.string, "soundPressure" : pitchEngine.signalLevel])
+//        self.sendEvent("onChangePitch", ["note" :  pitch.note.string, "soundPressure" : pitchEngine.signalLevel])
     }
 
     public func pitchEngine(_ pitchEngine: PitchEngine, didReceiveError error: Error) {
